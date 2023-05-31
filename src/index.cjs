67709af7fd9853e88292af65efef549a9b7043f7
bruwyvn <bruwyvn@outlook.com>
2023-05-31 19:48:15 -0300

    revert: meu primeiro commit
    This reverts commit c23fa92342c140a693d70a4e71019d6a5b867afc.

--- src/index.cjs
+++ src/index.cjs
@@ -1,4 +1,3 @@
-/*
 const express = require("express");
 const Chance = require("chance");
 
@@ -58,34 +57,3 @@ app.use("/", routes);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-*/
-const express = require('express')
-const app = express();
-const port = 3000;
-const sequelize = require('./src/system/config_db');
-
-await sequelize.authenticate();
-
-app.use('/usuario', require('./src/service/Usuario_service'));
-app.use('/compra', require('./src/service/Compra_service'));
-app.use('/fornecedor', require('./src/service/Fornecedor_service'));
-app.use('/material', require('./src/service/Material_service'));
-app.use('/transportadora', require('./src/service/Transportadora_service'));
-app.use('/venda', require('./src/service/Venda_service'));
-app.use('/cliente', require('./src/service/Cliente_service'));
-
-app.use('/usuarioAdm', require('./src/service_adm/Usuario_service'));
-app.use('/compraAdm', require('./src/service_adm/Compra_service'));
-app.use('/fornecedorAdm', require('./src/service_adm/Fornecedor_service'));
-app.use('/materialAdm', require('./src/service_adm/Material_service'));
-app.use('/transportadoraAdm', require('./src/service_adm/Transportadora_service'));
-app.use('/vendaAdm', require('./src/service_adm/Venda_service'));
-app.use('/clienteAdm', require('./src/service_adm/Cliente_service'));
-
-
-app.listen(port, ()=> {
-    console.log(`servidor iniciando na porta ${port}`)
-})
-
-
-
